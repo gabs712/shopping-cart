71cb38fa856b1d81e25bc500e4629e06dad92b74
gabs <gabs712.g@gmail.com>
2025-02-23 16:25:56 -0300

    Setup project structure

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,20 @@
-import App from './App'
+import Home from './pages/home/Home'
+import Root from './pages/root/Root'
+import Products from './pages/products/Products'
 
 export default [
   {
     path: '/',
-    element: <App />,
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/products',
+        element: <Products />,
+      },
+    ],
   },
 ]
