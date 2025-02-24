8c5420cc77ce1ad1ce01e93fc5f903381e1b2d6c
gabs <gabs712.g@gmail.com>
2025-02-24 14:45:53 -0300

    Add propTypes to Container

--- src/pages/root/Container.jsx
+++ src/pages/root/Container.jsx
@@ -1,7 +1,13 @@
+import PropTypes from 'prop-types'
+
 export default function Container({ children }) {
   return (
     <div className="w-full">
       <div className="mx-auto max-w-7xl">{children}</div>
     </div>
   )
+}
+
+Container.propTypes = {
+  children: PropTypes.element,
 }
