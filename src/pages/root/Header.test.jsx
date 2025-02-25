f67ee2a06848ea4adde530c7e9063aea60b58f2c
gabs <gabs712.g@gmail.com>
2025-02-25 00:59:02 -0300

    Test header highlights

---
+++ src/pages/root/Header.test.jsx
@@ -0,0 +1,44 @@
+import { describe, expect, it } from 'vitest'
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { BrowserRouter } from 'react-router-dom'
+import Header from './Header'
+
+describe('Header', () => {
+  it('Highlights current navlink', async () => {
+    render(<Header />, { wrapper: BrowserRouter })
+    const user = userEvent.setup()
+
+    const [home, products] = screen.getAllByRole('link')
+    const highlight = 'after:content-[""]'
+
+    expect(home).toHaveClass(highlight)
+    expect(products).not.toHaveClass(highlight)
+
+    await user.click(products)
+
+    expect(products).toHaveClass(highlight)
+    expect(home).not.toHaveClass(highlight)
+  })
+
+  it('Animates current highlight after clicking link', async () => {
+    render(<Header />, { wrapper: BrowserRouter })
+    const user = userEvent.setup()
+
+    const [home, products] = screen.getAllByRole('link')
+    const animation = 'after:animate-[increasingSize_400ms]'
+
+    expect(home).not.toHaveClass(animation)
+    expect(products).not.toHaveClass(animation)
+
+    await user.click(products)
+
+    expect(home).not.toHaveClass(animation)
+    expect(products).toHaveClass(animation)
+
+    await user.click(home)
+
+    expect(home).toHaveClass(animation)
+    expect(products).not.toHaveClass(animation)
+  })
+})
