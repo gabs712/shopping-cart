8821519506b68753ad6431cbf6e11396739d43ef
gabs <gabs712.g@gmail.com>
2025-02-27 14:45:25 -0300

    Add useFetchData hook

---
+++ src/hooks/useFetchData.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+
+export default function useFetchData(url) {
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    let ignore = false
+    async function fetchData() {
+      try {
+        const res = await fetch(url)
+        if (ignore) return
+        if (!res.ok) throw new Error('Server error')
+
+        setData(await res.json())
+      } catch (error) {
+        setError(error)
+      }
+    }
+
+    fetchData()
+
+    return () => {
+      ignore = true
+    }
+  }, [url])
+
+  return { data, error }
+}
