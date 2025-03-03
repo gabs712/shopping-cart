bc25970250e21bbc1d323d8193ece7b4015cb826
gabs <gabs712.g@gmail.com>
2025-03-03 17:37:43 -0300

    Add active style to CountButton

--- src/pages/root/CountButton.jsx
+++ src/pages/root/CountButton.jsx
@@ -1,7 +1,7 @@
 export default function CountButton({ char, onClick, className }) {
   return (
     <button
-      className="grid size-4 place-content-center rounded-full bg-slate-200"
+      className="grid active:opacity-60 transition-opacity size-4 place-content-center rounded-full bg-slate-200"
       aria-controls="cartCounter"
       type="button"
       onClick={(e) => {
