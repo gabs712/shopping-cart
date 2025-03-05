f3f4244a802f28e8b01a11198607296d60f2af7e
gabs <gabs712.g@gmail.com>
2025-03-05 02:19:27 -0300

    Add homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,1 +1,14 @@
-export default function Home() {}
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <div className="h-[75vh] gap-14 px-5 flex items-center justify-center flex-col">
+      <p className="text-brandHighlight text-center font-bold text-2xl">
+        Your favorite products for a low price.
+      </p>
+      <button className="bg-sky-500 px-2 hover:opacity-85 active:opacity-75 rounded-md py-1 text-white">
+        <Link to={'/products'}>Start now</Link>
+      </button>
+    </div>
+  )
+}
